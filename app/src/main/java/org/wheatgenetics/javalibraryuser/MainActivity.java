5216c32720142bfe1c45d10dc36ecdc1ad208076
Peter Rosario <PeterRosario@ksu.edu>
2017-06-16 12:27:14 -0500

    Tested Utils.adjust().

--- app/src/main/java/org/wheatgenetics/javalibraryuser/MainActivity.java
+++ app/src/main/java/org/wheatgenetics/javalibraryuser/MainActivity.java
@@ -18,11 +18,11 @@ public class MainActivity extends android.support.v7.app.AppCompatActivity
         super.onCreate(savedInstanceState);
         this.setContentView(org.wheatgenetics.javalibraryuser.R.layout.activity_main);
 
-        final int                     number   = 2;
-        final android.widget.TextView textView = (android.widget.TextView)
+        final java.lang.String        unadjusted = "  2 leading spaces";
+        final android.widget.TextView textView   = (android.widget.TextView)
             this.findViewById(org.wheatgenetics.javalibraryuser.R.id.textView);
         assert null != textView;
-        textView.setText(java.lang.String.format("doubleOf(%d) is %d",
-            number, org.wheatgenetics.javalib.Utils.doubleOf(number)));
+        textView.setText(java.lang.String.format("adjust(\"%s\") is \"%s\"",
+            unadjusted, org.wheatgenetics.javalib.Utils.adjust(unadjusted)));
     }
 }
