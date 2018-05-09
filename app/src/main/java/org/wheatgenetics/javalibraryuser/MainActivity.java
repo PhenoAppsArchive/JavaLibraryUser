bda34484e57ab5902ee9d3f6507954bd2cb05c27
Peter Rosario <PeterRosario@ksu.edu>
2018-05-09 11:53:23 -0500

    MainActivity: Added code to verify I am correctly using code in a library.

--- app/src/main/java/org/wheatgenetics/javalibraryuser/MainActivity.java
+++ app/src/main/java/org/wheatgenetics/javalibraryuser/MainActivity.java
@@ -4,6 +4,10 @@ package org.wheatgenetics.javalibraryuser;
  * Uses:
  * android.os.Bundle
  * android.support.v7.app.AppCompatActivity
+ * android.widget.TextView
+ *
+ * org.wheatgenetics.javalib.Utils
+ *
  * org.wheatgenetics.javalibraryuser.R
  */
 public class MainActivity extends android.support.v7.app.AppCompatActivity
@@ -11,6 +15,13 @@ public class MainActivity extends android.support.v7.app.AppCompatActivity
     @java.lang.Override protected void onCreate(final android.os.Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(org.wheatgenetics.javalibraryuser.R.layout.activity_main);
+        this.setContentView(org.wheatgenetics.javalibraryuser.R.layout.activity_main);
+
+        final java.lang.String        unadjusted = "  2 leading spaces";
+        final android.widget.TextView textView   =
+            this.findViewById(org.wheatgenetics.javalibraryuser.R.id.textView);
+        assert null != textView;
+        textView.setText(java.lang.String.format("adjust(\"%s\") is \"%s\"",
+            unadjusted, org.wheatgenetics.javalib.Utils.adjust(unadjusted)));
     }
 }
