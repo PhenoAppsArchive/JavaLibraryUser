f684a6029df9b1bb5e374661a44c61a50cb16300
Peter Rosario <PeterRosario@ksu.edu>
2018-05-23 12:33:22 -0500

    Dir: Added another constructor.  Added getPathAsString(). MainActivity: Tested Dir.

--- app/src/main/java/org/wheatgenetics/javalibraryuser/MainActivity.java
+++ app/src/main/java/org/wheatgenetics/javalibraryuser/MainActivity.java
@@ -6,6 +6,7 @@ package org.wheatgenetics.javalibraryuser;
  * android.support.v7.app.AppCompatActivity
  * android.widget.TextView
  *
+ * org.wheatgenetics.javalib.Dir
  * org.wheatgenetics.javalib.Utils
  *
  * org.wheatgenetics.javalibraryuser.R
@@ -17,11 +18,31 @@ public class MainActivity extends android.support.v7.app.AppCompatActivity
         super.onCreate(savedInstanceState);
         this.setContentView(org.wheatgenetics.javalibraryuser.R.layout.activity_main);
 
-        final java.lang.String        unadjusted = "  2 leading spaces";
-        final android.widget.TextView textView   =
-            this.findViewById(org.wheatgenetics.javalibraryuser.R.id.textView);
-        assert null != textView;
-        textView.setText(java.lang.String.format("adjust(\"%s\") is \"%s\"",
-            unadjusted, org.wheatgenetics.javalib.Utils.adjust(unadjusted)));
+        {
+            final java.lang.String        unadjusted     = "  2 leading spaces";
+            final android.widget.TextView adjustTextView =
+                this.findViewById(org.wheatgenetics.javalibraryuser.R.id.adjustTextView);
+            assert null != adjustTextView;
+            adjustTextView.setText(java.lang.String.format("adjust(\"%s\") is \"%s\"",
+                unadjusted, org.wheatgenetics.javalib.Utils.adjust(unadjusted)));
+        }
+
+        class Dir extends org.wheatgenetics.javalib.Dir
+        { private Dir(final java.io.File path) { super(path, ".javalibraryuser"); } }
+
+        org.wheatgenetics.javalib.Dir dir      = new Dir(this.getFilesDir());
+        android.widget.TextView       textView = this.findViewById(
+            org.wheatgenetics.javalibraryuser.R.id.internalDirTextView);
+        assert null != textView; textView.setText(dir.getPathAsString());
+
+        dir      = new Dir(this.getExternalFilesDir(null));
+        textView = this.findViewById(
+            org.wheatgenetics.javalibraryuser.R.id.externalPrivateDirTextView);
+        assert null != textView; textView.setText(dir.getPathAsString());
+
+        dir      = new Dir(android.os.Environment.getExternalStorageDirectory());
+        textView = this.findViewById(
+            org.wheatgenetics.javalibraryuser.R.id.externalPublicDirTextView);
+        assert null != textView; textView.setText(dir.getPathAsString());
     }
 }
